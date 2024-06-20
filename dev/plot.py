d488da42b8e8f99d1d314f4a89b7d6dada9db11d
Richard Lane <mh19137@it090783.users.bris.ac.uk>
2024-06-20 17:17:42 +0100

    plotting lib

---
+++ dev/plot.py
@@ -0,0 +1,25 @@
+"""
+Plotting helpers
+
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot_arr(arr: np.ndarray) -> tuple[plt.Figure, np.ndarray[plt.Axes]]:
+    """
+    Plot slices of a 3d array
+
+    """
+    indices = np.floor(np.arange(0, arr.shape[0], arr.shape[0] // 16)).astype(int)
+
+    fig, axes = plt.subplots(4, 4, figsize=(12, 12))
+    for i, ax in zip(indices, axes.flat):
+        ax.imshow(arr[i], cmap="gray")
+        ax.axis("off")
+        ax.set_title(i)
+
+    fig.tight_layout()
+
+    return fig, axes
