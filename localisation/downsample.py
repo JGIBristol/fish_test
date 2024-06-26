f2853d86adb98f3318fbb823956cbeae903b093b
Richard Lane <mh19137@it090783.users.bris.ac.uk>
2024-06-26 16:16:50 +0100

    maybe working downsample script

--- localisation/downsample.py
+++ localisation/downsample.py
@@ -22,9 +22,25 @@ def downsample(img_dir: pathlib.Path) -> None:
 
     """
     # Extract the image number
+    img_no = img_dir.name
+    path = f"{util.config()['downsampled_img_dir']}{img_no}.npy"
+
+    if os.path.exists(path):
+        print(f"Skipping {path}")
+        return
+
     # Read all the tifs into a numpy array
+    print(f"Reading {img_no}")
+    img_arr = image_io.read_tiffstack(int(img_no))
+
     # Downsample this array
+    rescaled_size = (512, 512, 512)
+    print(f"Downscaling {img_no}")
+    img_arr = resize(img_arr, rescaled_size, order=1)
+
     # Save it to disk
+    np.save(path, img_arr)
+    print(f"Saved {path}")
 
 
 def main():
@@ -32,16 +48,22 @@ def main():
     Downsample some images
 
     """
-    conf = util.config()
-    img_dir = pathlib.Path(image_io.user_config()["rdsf_dir"]) / conf["wahab_data_dir"]
+    img_dir = (
+        pathlib.Path(image_io.user_config()["rdsf_dir"])
+        / util.config()["wahab_data_dir"]
+    )
 
     dirs = list(img_dir.glob(r"[0-9][0-9][0-9]"))
 
+    if not os.path.exists(util.config()["downsampled_img_dir"]):
+        os.makedirs(util.config()["downsampled_img_dir"], exist_ok=True)
+
     # Only choose a few dirs for now
     gen = np.random.default_rng(seed=0)
-    dirs = gen.choice(dirs, size=5)
-    for dir in dirs:
-        print(dir)
+    dirs = gen.choice(dirs, size=12)
+
+    with Pool() as pool:
+        pool.map(downsample, dirs)
 
 
 if __name__ == "__main__":
