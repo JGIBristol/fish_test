a15a4f652ee32e9e8f09fe986365d8b9381fcd11
Richard Lane <mh19137@it090783.users.bris.ac.uk>
2024-06-28 11:22:26 +0100

    script for reading in images

--- save_img.py
+++ save_img.py
@@ -3,17 +3,28 @@ Cache the images to disk as numpy arrays
 
 """
 
+import os
 import argparse
 
-from dev import image_io
+import numpy as np
+
+from dev import image_io, util
 
 
 def main(img_n: int):
     """Read the selected image"""
+
+    cache_dir = util.config()["img_cache"]
+    if not os.path.exists(cache_dir):
+        os.makedirs(cache_dir)
+
+    arr = image_io.read_tiffstack(img_n)
+
+    np.save(f"{cache_dir}/{img_n}.npy", arr)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("img_n", type=int, help="The image number")
-    main(*parser.parse_args())
+    main(**vars(parser.parse_args()))
