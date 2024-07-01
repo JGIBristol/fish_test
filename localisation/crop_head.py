1126099db8d05e8936bf87ee26abb1c106c01dc7
Richard Lane <mh19137@it090783.users.bris.ac.uk>
2024-07-01 16:04:57 +0100

    plot

--- localisation/crop_head.py
+++ localisation/crop_head.py
@@ -13,7 +13,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-from dev import image_io, util, thresholding
+from dev import image_io, util, thresholding, plot as plot_lib
 
 
 def n_white_per_slice(img_arr: np.ndarray, threshold_pct: float) -> np.ndarray:
@@ -56,6 +56,23 @@ def avg_profile(
     return np.mean(shared_list, axis=0)
 
 
+def _plot_profile(profile: np.ndarray, plot_dir: str) -> None:
+    """
+    Plot the intensity profile
+
+    """
+    fig, axis = plt.subplots()
+
+    axis.plot(profile)
+    fig.suptitle("Number of white pixels per slice, averaged over thresholds")
+
+    axis.set_xlabel("Slice No")
+    axis.set_ylabel("Avg number of white pixels")
+
+    fig.tight_layout()
+    fig.savefig(f"{plot_dir}profile.png")
+
+
 def main(*, img_n: int, n_jobs: int, plot: bool):
     """
     Read in the image, count how many white pixels there are for various thresholds and take the average
@@ -66,22 +83,31 @@ def main(*, img_n: int, n_jobs: int, plot: bool):
     Then from the corresponding image slice, find the x/y window that contains the head and crop it out
 
     """
+    if plot:
+        plot_dir = f"plots/{img_n}/"
+        if not os.path.exists("plots"):
+            os.mkdir("plots")
+        os.mkdir(plot_dir)
+
     # Read in the image
     img_arr = image_io.read_tiffstack(
         img_n, n_jobs=None
     )  # Don't use threads here because it doesn't help
-    # TODO plot
+    if plot:
+        fig, _ = plot_lib.plot_stack(img_arr)
+        fig.savefig(f"{plot_dir}raw_stack.png")
 
     # Equalise the image, setting the number of pixels to be saturated
     img_arr = thresholding.equalise(img_arr, saturated_pct=0.35)
-    # TODO plot
+    if plot:
+        fig, _ = plot_lib.plot_stack(img_arr)
+        fig.savefig(f"{plot_dir}eq_stack.png")
 
     # For various thresholds, count the number of white pixels
     thresholds = [0.50, 0.65, 0.80, 0.95, 1.1, 1.25, 1.4]
     profile = avg_profile(img_arr, thresholds, n_jobs)
-    # TODO plot
-    plt.plot(profile)
-    plt.savefig("tmp.png")
+    if plot:
+        _plot_profile(profile, plot_dir)
 
     # Find peaks
     # Define our allowed regions and find where peaks are allowed
