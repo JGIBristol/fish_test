bbabddd70c349fd072ad7be6bb96b2c9357f59ff
Richard Lane <mh19137@it090783.users.bris.ac.uk>
2024-07-01 15:53:05 +0100

    script for finding the profile

--- localisation/crop_head.py
+++ localisation/crop_head.py
@@ -3,8 +3,60 @@ Crop out the fish head by looking at the greyscale intensity of the image and ma
 
 """
 
+import os
+import sys
+import pathlib
+import argparse
+from multiprocessing import Pool, Manager
 
-def main():
+import numpy as np
+import matplotlib.pyplot as plt
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from dev import image_io, util, thresholding
+
+
+def n_white_per_slice(img_arr: np.ndarray, threshold_pct: float) -> np.ndarray:
+    """
+    Find how many white pixels there are per slice with the given threshold
+
+    :param img_arr: The image array
+    :param threshold_pct: The threshold, as a quantile in percent
+
+    """
+    # Find the threshold
+    threshold = np.quantile(img_arr, 1 - (threshold_pct / 100))
+
+    # Count the number of white pixels per slice
+    return np.sum(img_arr > threshold, axis=(1, 2))
+
+
+def _n_white_per_slice(
+    img_arr: np.ndarray, threshold: float, shared_list: list
+) -> None:
+    shared_list.append(n_white_per_slice(img_arr, threshold))
+
+
+def avg_profile(
+    img_arr: np.ndarray, thresholds: list[float], n_jobs: int
+) -> np.ndarray:
+    """
+    For many thresholds, find the number of white pixels per slice
+    and take the average of them
+
+    """
+    shared_list = Manager().list()
+
+    with Pool(n_jobs) as pool:
+        pool.starmap(
+            _n_white_per_slice,
+            [(img_arr, threshold, shared_list) for threshold in thresholds],
+        )
+
+    return np.mean(shared_list, axis=0)
+
+
+def main(*, img_n: int, n_jobs: int, plot: bool):
     """
     Read in the image, count how many white pixels there are for various thresholds and take the average
 
@@ -15,11 +67,48 @@ def main():
 
     """
     # Read in the image
+    img_arr = image_io.read_tiffstack(
+        img_n, n_jobs=None
+    )  # Don't use threads here because it doesn't help
+    # TODO plot
+
+    # Equalise the image, setting the number of pixels to be saturated
+    img_arr = thresholding.equalise(img_arr, saturated_pct=0.35)
+    # TODO plot
+
     # For various thresholds, count the number of white pixels
+    thresholds = [0.50, 0.65, 0.80, 0.95, 1.1, 1.25, 1.4]
+    profile = avg_profile(img_arr, thresholds, n_jobs)
+    # TODO plot
+    plt.plot(profile)
+    plt.savefig("tmp.png")
+
     # Find peaks
     # Define our allowed regions and find where peaks are allowed
     # Choose a peak and find the corresponding slice
     # Choose the x/y window
 
+
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "img_n",
+        type=int,
+        help="The image number to crop the head from",
+    )
+    parser.add_argument(
+        "--n_jobs",
+        "-n",
+        type=int,
+        default=6,
+        help="Number of threads to use for thresholding.",
+    )
+    parser.add_argument(
+        "--plot",
+        action="store_true",
+        help="Whether to plot the images at each stage of the process",
+        default=False,
+    )
+
+    main(**vars(parser.parse_args()))
