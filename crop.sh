31b56b31a290e9ca9a4195eb89703d9e862cc423
Richard Lane <mh19137@it090783.users.bris.ac.uk>
2024-07-03 16:17:07 +0100

    list of test fish

--- crop.sh
+++ crop.sh
@@ -4,7 +4,13 @@ set -e
 
 # Some wildtypes
 max_jobs=6
-for i in 40 42 47 53 61 70 89 95 99 201 205 218 224 230 240 243 247 363 372 373 384 389 441 490 545 575;
+
+# All the ones I looked at initially
+fish_ids=(40 42 47 53 61 70 89 95 99 201 205 218 224 230 240 243 247 363 372 373 384 389 441 490 545 575)
+
+# The ones I'm using to fine-tune the model
+test_fish=(40 95 218 247 372 389 53 230 545)
+for i in "${test_fish[@]}";
 do
 	# If any files called cropped/i/sub_window* exist, skip
 	if compgen -G "cropped/$i/sub_window*" > /dev/null; then
