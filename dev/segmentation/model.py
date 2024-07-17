f26f9b5545b1c946d7f1222f8f88afa762786337
Richard Lane <mh19137@it090783.users.bris.ac.uk>
2024-07-17 14:45:50 +0100

    network parameters

--- dev/segmentation/model.py
+++ dev/segmentation/model.py
@@ -3,15 +3,14 @@ Model for performing segmentation
 
 """
 
-import monai
-import monai.networks
 from monai.networks.nets import AttentionUnet
 
+from . import util
 
-# Attention U-Net from monai
-def model(params: dict) -> AttentionUnet:
+
+def model() -> AttentionUnet:
     """
     U-Net model for segmentation
 
     """
-    return AttentionUnet(spatial_dims=3, in_channels=1, out_channels=
+    return AttentionUnet(**util.model_params())
