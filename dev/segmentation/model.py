7257d917269c90d6feac5cb88e8f53ee103a02cd
Richard Lane <mh19137@it090783.users.bris.ac.uk>
2024-07-17 15:52:22 +0100

    create image of the model architecture

--- dev/segmentation/model.py
+++ dev/segmentation/model.py
@@ -3,6 +3,8 @@ Model for performing segmentation
 
 """
 
+import torch
+from torchviz import make_dot
 from monai.networks.nets import AttentionUnet
 
 from . import util
@@ -14,3 +16,25 @@ def model() -> AttentionUnet:
 
     """
     return AttentionUnet(**util.model_params())
+
+
+def draw_model(model: AttentionUnet, path: str) -> None:
+    """
+    Create an image of the architecture of the model at the given path
+    Also creates a dot file
+
+    :param: the model
+    :param: the path to save the image, e.g. "model.png"
+
+    """
+    path, fmt = path.rsplit(".", 1)
+
+    # Add dummy input to the model to generate the image
+    params = util.util.userconf()["model_params"]
+    dummy_input = torch.randn(
+        1, params["in_channels"], *[128 for _ in range(params["spatial_dims"])]
+    )
+
+    dot = make_dot(model(dummy_input), params=dict(model.named_parameters())).render(
+        path, format=fmt
+    )
