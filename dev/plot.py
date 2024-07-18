4fa460051fac9fb942789fc9e2d413f5f305ec6b
Richard Lane <mh19137@it090783.users.bris.ac.uk>
2024-07-18 13:48:24 +0100

    same colormap for all images

--- dev/plot.py
+++ dev/plot.py
@@ -13,10 +13,11 @@ def plot_arr(arr: np.ndarray) -> tuple[plt.Figure, np.ndarray[plt.Axes]]:
 
     """
     indices = np.floor(np.arange(0, arr.shape[0], arr.shape[0] // 16)).astype(int)
+    vmin, vmax = np.min(arr), np.max(arr)
 
     fig, axes = plt.subplots(4, 4, figsize=(12, 12))
     for i, ax in zip(indices, axes.flat):
-        ax.imshow(arr[i], cmap="gray")
+        ax.imshow(arr[i], cmap="gray", vmin=vmin, vmax=vmax)
         ax.axis("off")
         ax.set_title(i)
 
